f472d7f14562f572739ef5104eece4155cfda841
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-03-21 19:13:20 +0530

    cors conflict

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,7 +7,7 @@ import todoRoutes from "./routes/TodoRoutes.js"
 dotenv.config();
 app.use(cors(
     {
-        origin: "*",
+        origin: ["https://minetasklist.vercel.app/"],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     }
 
